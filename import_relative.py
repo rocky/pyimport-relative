d925c89546dff8fcb511c97b80e0e9c76a9dd052
rocky <rocky@gnu.org>
2013-03-24 02:33:29 -0400

    Give a more informative messages when we can't find an import. Go over packaging.

--- import_relative.py
+++ import_relative.py
@@ -1,42 +1,53 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2009 Rocky Bernstein
+#   Copyright (C) 2009, 2013 Rocky Bernstein
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-#    02110-1301 USA.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import imp, os, re, sys
 def get_srcdir(level=1):
-    ''' Get directory of caller as an absolute file name. `level' is
+    """Get directory of caller as an absolute file name. *level* is
     the number of frames to look back.  So for import file which is
     really doing work on behalf of *its* caller, we go back 2.
 
     NB: f_code.co_filenames and thus this code kind of broken for
     zip'ed eggs circa Jan 2009
-    '''
+    """
+
     caller = sys._getframe(level)
     filename = caller.f_code.co_filename
     filename = os.path.normcase(os.path.dirname(os.path.abspath(filename)))
     return os.path.realpath(filename)
 
+def get_src(level=1):
+    """Get caller filename as an absolute file name. *level* is
+    the number of frames to look back.  So for import file which is
+    really doing work on behalf of *its* caller, we go back 2.
+
+    NB: f_code.co_filenames and thus this code kind of broken for
+    zip'ed eggs circa Jan 2009
+    """
+    caller = sys._getframe(level)
+    filename = caller.f_code.co_filename
+    filename = os.path.normcase(os.path.abspath(filename))
+    return os.path.realpath(filename)
+
+
 def get_namespace(top_name, srcdir):
-    '''Return a compount 'import' based on srcdir and to stop
-    `top_name'.  Note: we assume packages don't have subdirectories
+    """Return a compound 'import' string e.g. "a.b.c" based on *srcdir* and
+    *top_name*.  Note: we assume packages don't have subdirectories
     with the same name as the package. E.g. pydbg.pydbg or
-    pydbg.foo.pydbg is forbidden.  One remove that and by disallowing
-    the package name in the prefix of the directory but that does occur
-    e.g. /src/svn/pydbg/pydbg/lib. 
-    '''
+    pydbg.foo.pydbg is forbidden.
+    """
     srcdirs = srcdir.split(os.path.sep)
     if top_name in srcdirs:
         srcdirs.reverse()
@@ -58,33 +69,33 @@ def path2abspath(path, call_level):
         srcdir = path
     else:
         # Check for ., .., ...
-        if '.' == path[0]: 
+        if '.' == path[0]:
             alldots = True
             i = 1
             pardir = '.'
             for i in range(1,len(path)):
-                if path[i] != '.': 
+                if path[i] != '.':
                     path = path[i:]
                     alldots = False
                     break
                 pardir = os.path.join(pardir, os.path.pardir)
                 pass
             if alldots:
-                srcdir = os.path.abspath(os.path.join(get_srcdir(call_level), 
+                srcdir = os.path.abspath(os.path.join(get_srcdir(call_level),
                                                       pardir))
             else:
-                srcdir = os.path.abspath(os.path.join(get_srcdir(call_level), 
+                srcdir = os.path.abspath(os.path.join(get_srcdir(call_level),
                                                       pardir, path))
                 pass
             pass
         else:
-            srcdir = os.path.abspath(os.path.join(get_srcdir(call_level), 
+            srcdir = os.path.abspath(os.path.join(get_srcdir(call_level),
                                                       '.', path))
         pass
     return srcdir
 
 def import_relative(import_name, path=None, top_name=None):
-    '''Import `import_name' using `path' as the location to start
+    """Import `import_name' using `path' as the location to start
     looking for it.  If `path' is not given, we'll look starting in
     the directory where the import_relative was issued. In contrast to
     __import__() which this uses, we alway return the last import
@@ -92,7 +103,7 @@ def import_relative(import_name, path=None, top_name=None):
     don't do "from lists", global or local variables here.
 
     TODO: add a package/namespace parameter for which to add the name under.
-    '''
+    """
 
     srcdir = path2abspath(path, 3)
 
@@ -118,9 +129,12 @@ def import_relative(import_name, path=None, top_name=None):
 
         # If any of the following calls raises an exception,
         # there's a problem we can't handle -- let the caller handle it.
-        
-        fp, pathname, description = imp.find_module(top_module, [srcdir])
-        
+
+        try:
+            fp, pathname, description = imp.find_module(top_module, [srcdir])
+        except ImportError:
+            raise ImportError("No module %s found in %s" %
+                              (top_module, srcdir))
         module_save = None
         if sys.modules.get(namespaced_top_module):
             # Temporarily nuke module so we will have to find it anew using
@@ -133,7 +147,7 @@ def import_relative(import_name, path=None, top_name=None):
             pass
         try:
             try:
-                mod = imp.load_module(namespaced_top_module, fp, pathname, 
+                mod = imp.load_module(namespaced_top_module, fp, pathname,
                                       description)
             except (SystemError, ImportError):
                 mod = imp.load_module(top_module, fp, pathname, description)
@@ -158,11 +172,11 @@ def import_relative(import_name, path=None, top_name=None):
     # understand 'knee' from:
     # http://docs.python.org/library/imputil.html better. (It seems
     # to be a translation into Python of its C code).
-    # 
+    #
     # Until then, So we'll use this hacky method which doesn't deal
     # with "from .. import .. as .." renaming which would need to look
     # at __name__.
-    
+
     prefix = namespaced_top_module
     prev   = mod
     del import_modules[0]
@@ -196,7 +210,7 @@ def import_relative(import_name, path=None, top_name=None):
                 del sys.modules[prefix]
                 pass
             pass
-        
+
         if hasattr(next_mod, '__path__'):
             np = next_mod.__path__
             np[0], np[-1] = np[-1], np[0]
@@ -214,7 +228,7 @@ if __name__=='__main__':
     Mtest = import_relative('test.os2.path', '.', 'import-relative')
 
     print(get_namespace('pydbg', '/src/pydbg/pydbg/processor/commands'))
-    
+
     Mimport_relative = import_relative('import_relative')
     print(Mimport_relative)
     print(Mimport_relative.__name__)
@@ -224,7 +238,7 @@ if __name__=='__main__':
     import_relative2 = Mimport_relative.import_relative('import_relative',
                                                         '.')
 
-    
+
     # Originally done with os.path, But nosetest seems to use this.
     os2_path = Mimport_relative.import_relative('os2.path', 'test',
                                                 'import_relative')
